ca8d5b60e0319ff11b053e51beeb2e4746bec2c2
sachiokuwahata <sachio.kuwahata@gmail.com>
2020-09-22 14:56:45 +0900

    test6

--- tests/Feature/Database/DatabaseTest.php
+++ tests/Feature/Database/DatabaseTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature\Database;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Book;
+use App\Models\Book;
 
 class DatabaseTest extends TestCase
 {
