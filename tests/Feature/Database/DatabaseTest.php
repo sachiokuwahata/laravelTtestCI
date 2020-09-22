c418093078bbae73572b78afd234f4093bf455be
sachiokuwahata <sachio.kuwahata@gmail.com>
2020-09-22 14:52:22 +0900

    test5

---
+++ tests/Feature/Database/DatabaseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Database;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Book;
+
+class DatabaseTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testDatabase()
+    {
+        $book = new Book();
+        $book->title = 'hoge';
+        $book->author = 'tarou';
+        $saveBook = $book->save();
+ 
+        $book = [
+            'title' => 'hoge',
+        ];
+        $this->assertDatabaseHas('books', $book);
+    }
+}
